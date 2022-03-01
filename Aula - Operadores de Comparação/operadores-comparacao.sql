66e8939a09823e7fc1b66a600a3d0f47cfb63ece
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-03-01 12:44:37 -0300

    Aula de operadores de comparação (fim)

--- Aula - Operadores de Comparação/operadores-comparacao.sql
+++ Aula - Operadores de Comparação/operadores-comparacao.sql
@@ -1,3 +1,36 @@
 --USO DO OPERADOR =
-	SELECT * FROM CIDADES
+	SELECT * FROM cidades
 	WHERE uf = 'SP'
+
+	SELECT * FROM cidades
+	WHERE nome_mun = 'Dourado' AND uf = 'SP'
+
+--Uso do operador >
+	SELECT * FROM cidades
+	WHERE populacao > 100000
+
+	SELECT * FROM cidades
+	WHERE populacao > 100000 ORDER BY populacao ASC
+
+--Uso do operador <
+	SELECT * FROM cidades
+	WHERE populacao < 50000 ORDER BY populacao ASC
+
+--Uso de operador <=
+	SELECT * FROM cidades
+	WHERE populacao <= 10000 ORDER BY populacao ASC
+
+--Uso de operador >=
+	SELECT * FROM cidades
+	WHERE populacao >= 10000 ORDER BY populacao ASC
+
+--Uso do operador <>
+	SELECT * FROM cidades
+	WHERE uf <> 'SP' AND uf <> 'SC'
+
+--EXEMPLO COMBINANDO OPERADORES
+	SELECT * FROM cidades
+	WHERE populacao >= 50000
+	AND populacao <= 100000
+	AND uf = 'SP'
+	AND nome_mun <> 'Vinhedo'
