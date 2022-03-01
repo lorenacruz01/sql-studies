c4949cca4774fb864295c31801648aa377facc5a
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-03-01 12:57:22 -0300

    Aula de operadores aritméticos - Parte 1

---
+++ Aula - Operadores Aritméticos/operadores-aritmeticos.sql
@@ -0,0 +1,18 @@
+--Uso do operador +
+	SELECT 1 + 1
+
+--Uso do operador *
+	SELECT 3 * 2
+
+--Uso do operador -
+	SELECT 5 - 3
+
+--Uso do operador /
+	SELECT 15 / 3
+
+--Uso do operador %
+	SELECT 12 % 5
+
+--EXEMPLO EXPRESSÃO
+
+	SELECT (1 + 4) * (12 - 3)
