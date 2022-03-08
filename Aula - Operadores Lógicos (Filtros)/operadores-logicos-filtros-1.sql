903e13c5ff6ff546075ff54f3874e89148e8183d
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-03-08 00:42:36 -0300

    Aula - Operadores lógicos/filtros (parte 3)

--- Aula - Operadores Lógicos (Filtros)/operadores-logicos-filtros-1.sql
+++ Aula - Operadores Lógicos (Filtros)/operadores-logicos-filtros-1.sql
@@ -22,5 +22,46 @@ USE AdventureWorks2014
 USE AdventureWorks2014
 	 SELECT p.firstname,
 			p.lastname,
-	 FROM Person.Person AS p
-	  
+			ep.Rate
+	 FROM Person.Person p
+	 JOIN HumanResources.EmployeePayHistory ep
+	 ON p.BusinessEntityID = ep.BusinessEntityID
+		WHERE ep.Rate BETWEEN 27 AND 30
+	ORDER BY ep.Rate
+
+--Uso do operador IN
+USE AdventureWorks2014
+	 SELECT p.FirstName,
+			p.Lastname,
+			sp.salesquota
+	 FROM	Person.Person AS p
+	 JOIN	Sales.SalesPerson AS sp
+	 ON p.BusinessEntityID = sp.BusinessEntityID
+	 WHERE P.BusinessEntityID IN (SELECT BusinessEntityID FROM Sales.SalesPerson WHERE SalesQuota > 250000)
+
+--Uso do operador NOT IN
+USE AdventureWorks2014
+	 SELECT p.FirstName,
+			p.Lastname,
+			sp.salesquota
+	 FROM	Person.Person AS p
+	 JOIN	Sales.SalesPerson AS sp
+	 ON p.BusinessEntityID = sp.BusinessEntityID
+	 WHERE P.BusinessEntityID NOT IN (SELECT BusinessEntityID FROM Sales.SalesPerson WHERE SalesQuota > 250000)
+
+--Uso do operador Like: localizar valores que contenham "or" em qualquer lugar
+USE NORTHWND
+	SELECT * FROM Customers
+		WHERE ContactName LIKE '%or%'
+
+--Uso do operador Like: encontrar quaisquer valores que contenham "r" na segunda posição
+	SELECT * FROM Customers
+		WHERE ContactName LIKE '_r%'
+
+--Uso do operador Like: localizar valores que começam com 'a' e que possuem pelo menos 3 caracteres de comprimento
+	SELECT * FROM Customers
+		WHERE ContactName LIKE 'a_%_%'
+
+--Ùso do operador like: localizar valores que começam com 'a' e que terminam com 'o'
+	SELECT * FROM Customers
+		WHERE ContactName LIKE 'a%o'
