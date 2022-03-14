89ef50edfc234a827783966b64e47127ca10e893
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-03-14 14:15:09 -0300

    Data definition language - parte 3

--- DDL (Data Defition Language)/data-definition-language.sql
+++ DDL (Data Defition Language)/data-definition-language.sql
@@ -19,3 +19,27 @@ CREATE TABLE Salario(
 --corrigindo o erro da tabela salario: COMANDO PARA ADICIONAR CHAVE PRIMÁRIA A UMA TABELA JÁ CRIADA
 
 ALTER TABLE Funcionario ADD PRIMARY KEY (MATRICULA)
+
+--DDL CRIAÇÃO DE TABELA COM CHAVE PRIMÁRIA
+CREATE TABLE audit_salario(
+	transacao INT IDENTITY(1,1),
+	matricula INT NOT NULL,
+	data_trans DATETIME NOT NULL,
+	sal_antigo DECIMAL (10,2),
+	sal_novo DECIMAL(10,2),
+	usuario	VARCHAR(20) NOT NULL
+)
+--DDL: ADICIONANDO COLUNA A TABELA JA CRIADA
+ALTER TABLE Funcionario ADD data_nasc DATETIME
+--DDL: CRIAÇÃO DE INDEX
+select * from Funcionario
+CREATE INDEX ix_func1 ON Funcionario(data_nasc)
+CREATE INDEX ix_func2 ON Funcionario(cidade, pais)
+
+--DDL: Adicionando novo campo (coluna) a uma tabela já criada
+ALTER TABLE Funcionario ADD genero CHAR(1)
+
+--Utilizando procedure nativa
+--Renomeando campo da tabela
+--EXEC sp_rename 'TabelaOrigem.CampoOrigem', 'campo_origem', 'COLUMN'
+EXEC sp_rename 'Funcionario.endereco', 'ender', 'COLUMN'
