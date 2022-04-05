dea1e9321e78e098946a20844b1611010f7346b2
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-04-05 17:11:08 -0300

    Exercícios com Funções de Agregação (Parte 1)

---
+++ Funções de Agregação/exercicios-funcoes-de-agregacao.sql
@@ -0,0 +1,22 @@
+--EXERCÍCIOS DE FUNÇÃO DE AGREGAÇÃO
+
+--conhecendo as tabelas
+USE curso
+SELECT * FROM cidades
+SELECT * FROM senso_2013
+SELECT * FROM uf
+SELECT * FROM regiao_uf
+
+--AVG: RETORNA A MÉDIA DOS VALORES DE UM GRUPO. IGNORA OS VALORES NULOS.
+SELECT AVG(populacao) AS 'MEDIA POPULACIONAL' FROM cidades
+--AVG POR ESTADO
+SELECT uf AS ESTADO, AVG(populacao) AS 'MEDIA POPULACIONAL' FROM cidades
+GROUP BY UF 
+ORDER BY 2
+
+select cod_uf from uf where estado = 'Paraná'
+select cod_uf from uf where estado IN (select estado from regiao_uf where estado='Paraná')
+
+--MIN: retorna o valor mínimo na expressão. Pode ser seguido da cláusula OVER
+SELECT MIN(populacao) AS TOTAL FROM cidades
+SELECT nome_mun, populacao AS TOTAL FROM cidades WHERE populacao IN (SELECT MIN(populacao) AS TOTAL FROM cidades)
