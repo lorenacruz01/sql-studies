683cacb3d8e5bb61b2ebe9f64aff0baa35d3284f
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-07-05 22:07:22 -0300

    Expressão Case - Parte 2

--- Case/case-expression.sql
+++ Case/case-expression.sql
@@ -41,3 +41,43 @@ SELECT ProductNumber,
 	  Name
 FROM  Production.Product
 ORDER BY ProductNumber
+
+
+--EXEMPLO
+USE AdventureWorks2014
+SELECT ProductNumber,
+	   Name,
+	   ListPrice,
+	   CASE
+		  WHEN ListPrice = 0 THEN 'Não está a venda'
+		  WHEN ListPrice < 50 THEN 'Abaixo de $50'
+		  WHEN ListPrice >= 50 
+			AND ListPrice < 250 THEN 'Abaixo de $250'
+		  WHEN ListPrice >= 250 
+		    AND ListPrice < 1000 THEN 'Abaixo de $1000'
+		  ELSE 'Acima de $1000'
+	   END PriceRange,
+       'Produtos' AS Categoria
+FROM   Production.Product
+ORDER  BY ProductNumber
+
+--Utilizando o case para ordenar uma consulta
+SELECT BusinessEntityId,
+	   SalariedFlag 
+FROM   HumanResources.Employee
+ORDER  BY CASE WHEN SalariedFlag = 1 THEN BusinessEntityID END DESC,
+		  CASE WHEN SalariedFlag = 0 THEN BusinessEntityID END ASC
+
+--UPDATE COM CASE
+BEGIN TRANSACTION
+	UPDATE HumanResources.Employee
+	SET VacationHours = (CASE WHEN ((VacationHours - 10.00) < 0) 
+	THEN VacationHours + 40
+	ELSE (VacationHours + 20) END)
+
+	output deleted.BusinessEntityID,
+		   deleted.VacationHours as 'antes',
+		   inserted.VacationHours as 'depois'
+	WHERE SalariedFlag = 0;
+
+ROLLBACK TRANSACTION
