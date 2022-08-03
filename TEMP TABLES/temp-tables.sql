dec5330f00950c725e09ab351af8bc5f7c71a690
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-08-02 21:42:47 -0300

    Exercícios com tabelas temporárias (Parte 2)

--- TEMP TABLES/temp-tables.sql
+++ TEMP TABLES/temp-tables.sql
@@ -14,3 +14,26 @@ INSERT INTO #mytemptable1 VALUES ('euro', 3000)
 --selecionando dados de uma tabela temporária local
 SELECT * FROM #mytemptable1
 SELECT field1, field2 FROM #mytemptable1
+
+--criando uma tabela temporária por meio de um select
+SELECT * INTO #mytemptable2 FROM #mytemptable1 
+--checando os dados
+SELECT * FROM #mytemptable1
+SELECT * FROM #mytemptable2
+
+--atualizando tabela temporária
+UPDATE #mytemptable2 SET field2 = '5.17' WHERE field1 = 'dolar'
+UPDATE #mytemptable2 SET field2 = '5.29' WHERE field1 = 'euro'
+
+--deletando registros de uma tabela temporária
+DELETE FROM #mytemptable1
+
+--dropando a tabela temporária
+DROP TABLE #mytemptable1
+DROP TABLE #mytemptable2
+
+--EXEMPLO
+USE CURSO
+SELECT nome_mun INTO #mytemptable3 FROM cidades
+
+SELECT * FROM #mytemptable3
