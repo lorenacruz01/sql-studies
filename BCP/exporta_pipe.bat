dbf33ef4beb41688c4d7b315c4456189411372d1
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-12-11 23:53:01 -0300

    BCP continuação

---
+++ BCP/exporta_pipe.bat
@@ -0,0 +1,2 @@
+bcp CURSO.DBO.CLIENTES OUT C:\workspace\sql-studies\BCP\export_test\export_result_pipe.txt -o C:\workspace\sql-studies\BCP\export_test\logs\export.log -T -c -r "\n" -t "|" -c
+PAUSE
---
+++ BCP/exporta_pipe_data.bat
@@ -0,0 +1,2 @@
+bcp CLIENTES OUT C:\workspace\sql-studies\BCP\export_test\export_result_pipe_data.txt -o C:\workspace\sql-studies\BCP\export_test\logs\export_pipe_data.log -d curso -T -c -r "\n" -t "|" -c
+PAUSE
