e23ebaee368df7d6a8122cc92fa0f3cce4c3fb2e
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2023-02-19 17:18:29 -0300

    cursores (parte 2)

--- Cursores/cursores.sql
+++ Cursores/cursores.sql
@@ -12,3 +12,46 @@ WHILE(@@FETCH_STATUS = 0)
 	PRINT 'FETCH STATUS -> ' + CAST(@@FETCH_STATUS AS VARCHAR(10))
 	CLOSE meuCursor
 	DEALLOCATE meuCursor
+
+--cursor realizando update
+USE curso
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'clifor')
+	DROP TABLE clifor
+GO
+SELECT a.BusinessEntityID codPessoa,
+	   a.FirstName nome,
+	   a.LastName sobrenome,
+	   cast('' as varchar(100)) nomeCompleto
+	   into clifor
+FROM   AdventureWorks2014.person.person a
+
+--VERIFICANDO DADOS CARREGADOS SEM NOME COMPLETO
+SELECT * FROM CLIFOR
+--INÍCIO DO CURSOR (DECLARANDO VARIÁVEIS)
+DECLARE @codpessoa INT,
+		@nome VARCHAR(50),
+		@sobrenome VARCHAR(50),
+		@nomecompleto VARCHAR(100)
+--DECLARANDO O CURSOR PARA PERCORRER OS REGISTROS
+DECLARE cursor1 
+CURSOR LOCAL FOR
+	SELECT codPessoa,
+		   nome,
+		   sobrenome
+	FROM   clifor
+--ABRINDO O CURSOR
+OPEN cursor1
+--LENDO A PRÓXIMA LINHA
+FETCH NEXT FROM cursor1 INTO @codpessoa, @nome, @sobrenome
+--PERCORRENDO AS LINHAS DO CURSOR
+WHILE @@FETCH_STATUS = 0
+	BEGIN
+		UPDATE clifor
+		SET nomecompleto = @nome + ' ' + @sobrenome
+		WHERE codpessoa = @codpessoa
+		FETCH NEXT FROM cursor1 INTO @codpessoa, @nome, @sobrenome
+	END
+--FECHANDO O CURSOR E DESALOCANDO OS RECURSOS
+CLOSE cursor1 
+DEALLOCATE cursor1
+SELECT * FROM clifor
